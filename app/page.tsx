d2a36825dad2ef5bf1f8d58ceedffc8dab43ed14
ヒトリユウ <yuu.hitorin@gmail.com>
2024-07-14 16:15:07 +0900

    [EDIT] [ADD]

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { auth } from "@/auth";
+import CustomLink from "@/components/custom-link";
 
-const liffId = '2004758018-agjl6Vpq';
-
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
+    <div className="flex flex-col gap-6">
+      <h1 className="text-3xl font-bold">🚀NextAuth.js Tutorial</h1>
+      <div>
+        <CustomLink href="/server-example" className="underline">
+          サーバー
+        </CustomLink>
+        と
+        <CustomLink href="/client-example" className="underline">
+          クライアント
+        </CustomLink>
+        の例を見て、ページを保護してセッションデータを取得する方法を確認してください。
+      </div>
+      <div className="flex flex-col rounded-md bg-neutral-100">
+        <div className="p-4 font-bold rounded-t-md bg-neutral-200">
+          Current Session
+        </div>
+        <pre className="py-6 px-4 whitespace-pre-wrap break-all">
+          {JSON.stringify(session, null, 2)}
+        </pre>
+      </div>
+    </div>
   );
 }

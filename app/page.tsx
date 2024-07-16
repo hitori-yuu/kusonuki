b31061f5df1b46f90138d2b4619647aeca6c9d4a
ヒトリユウ <yuu.hitorin@gmail.com>
2024-07-16 22:06:13 +0900

    [EDIT] [ADD] delete old components add Header, Footer, etc..

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,18 @@
-import { auth } from "@/auth";
-import CustomLink from "@/components/custom-link";
+import Timetable from "@/components/layouts/Timetable";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 
 export default async function Home() {
-  const session = await auth();
-
   return (
-    <div className="flex flex-col gap-6">
-      <h1 className="text-3xl font-bold">🚀NextAuth.js Tutorial</h1>
-      <div>
-        <CustomLink href="/server-example" className="underline">
-          サーバー
-        </CustomLink>
-        と
-        <CustomLink href="/client-example" className="underline">
-          クライアント
-        </CustomLink>
-        の例を見て、ページを保護してセッションデータを取得する方法を確認してください。
-      </div>
-      <div className="flex flex-col rounded-md bg-neutral-100">
-        <div className="p-4 font-bold rounded-t-md bg-neutral-200">
-          Current Session
-        </div>
-        <pre className="py-6 px-4 whitespace-pre-wrap break-all">
-          {JSON.stringify(session, null, 2)}
-        </pre>
-      </div>
+    <div className="min-h-screen bg-white">
+      <Timetable />
     </div>
-  );
+  )
 }
---
+++ components/layouts/Timetable.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+const timetables = [
+    {
+        "period": "1",
+        "subject": "Math",
+        "change": "",
+    },
+    {
+        "period": "2",
+        "subject": "Math",
+        "change": "",
+    },
+    {
+        "period": "3",
+        "subject": "Math",
+        "change": "AAAA",
+    },
+    {
+        "period": "4",
+        "subject": "Math",
+        "change": "",
+    },
+    {
+        "period": "5",
+        "subject": "Math",
+        "change": "",
+    },
+]
+
+const Timetable = () => {
+  return (
+    <Card>
+        <CardHeader>
+            <CardTitle>Timetable</CardTitle>
+            <CardDescription>A week 7/21</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <Table>
+                <TableBody>
+                    {timetables.map((timetable) => (
+                    <TableRow key={timetable.period}>
+                        <TableCell className="font-bold">{timetable.period}</TableCell>
+                        {timetable.change ? timetable.change &&
+                        <TableCell className="text-red-600">
+                            <Popover>
+                                <PopoverTrigger>{timetable.change}</PopoverTrigger>
+                                <PopoverContent><p className="text-center">前: {timetable.subject}</p></PopoverContent>
+                            </Popover>
+                        </TableCell>
+                        : timetable.subject &&<TableCell>{timetable.subject}</TableCell>}
+                    </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default Timetable

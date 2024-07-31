f57005e5317b349706a83035c962ab7eb3eeb96a
ヒトリユウ <yuu.hitorin@gmail.com>
2024-07-31 14:55:44 +0900

    [EDIT] [ADD] create profile page

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,14 +2,13 @@ import prisma from "@/lib/prismaClient"
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request,
-    { params }: { params: { userId: string }}
+    { params }: { params: { id: string }}
 ) {
-    const id = params.userId
+    const id = params.id
     const userData = await prisma.user.findUnique({
         where: {
             id: id,
         }
     });
-    console.log(userData);
     return NextResponse.json(userData)
 };

3e1b7682479c296142c8b6ca0d942e6980dba97d
ヒトリユウ <yuu.hitorin@gmail.com>
2024-08-07 15:14:10 +0900

    [DEL] [EDIT] delete DataTable.tsx Edit Table design for mobile

--- components/layouts/table/FacetedFilter.tsx
+++ components/layouts/table/FacetedFilter.tsx
@@ -38,7 +38,7 @@ export function DataTableFacetedFilter<TData, TValue>({
 	return (
 		<Popover>
 			<PopoverTrigger asChild>
-				<Button variant="outline" size="sm" className="h-8 border-dashed">
+				<Button variant="outline" size="sm" className="h-8 border-dashed mr-1">
 					<PlusCircledIcon className="mr-2 h-4 w-4" />
 					{title}
 					{selectedValues?.size > 0 && (

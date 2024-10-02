3cdfec19b56bde8e99464b0b6d7019f0859a3a75
ヒトリユウ <yuu.hitorin@gmail.com>
2024-10-02 21:00:47 +0900

    [EDIT] route.ts

--- app/api/timetables/route.ts
+++ app/api/timetables/route.ts
@@ -31,11 +31,7 @@ export async function POST(req: Request) {
 		fourth,
 		fifth,
 		authorId,
-		createdAt,
-		updatedAt,
 	} = await req.json();
-	const created_at = new Date(createdAt);
-	const updated_at = new Date(updatedAt);
 
 	const timetable = await prisma.timetable.create({
 		data: {
@@ -50,8 +46,6 @@ export async function POST(req: Request) {
 			fourth,
 			fifth,
 			authorId,
-			createdAt: created_at,
-			updatedAt: updated_at,
 		},
 	});
 	return NextResponse.json(timetable);
